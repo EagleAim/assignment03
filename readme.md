9946b4ac3dd106b1d6d419a557007fb21baeef40
Logan King <59426296+EagleAim@users.noreply.github.com>
2023-11-05 23:21:37 -0800

    question 1 complete

---
+++ readme.md
@@ -0,0 +1,2 @@
+Author: Logan King
+Purpose: This project is ment to show that I can generate commands in the console.
--- script1.js
+++ script1.js
@@ -7,3 +7,15 @@
 // ###
 // ##
 // #
+
+let n=4;
+let x=3;
+for (let i = 1; i <= n; i++) { 
+    let str = "#"; 
+    console.log(str.repeat(i)); 
+}
+
+for (let i = x; i >= 1; i--) {
+    let str ="#";
+    console.log(str.repeat(i));
+}
